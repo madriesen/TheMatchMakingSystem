18ffc79741a1f9401e65362e6afa3ed0452e45cb
Ayman <r0746124@student.thomasmore.be>
2020-12-07 11:28:22 +0100

    fout oplossen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from '../app/app.component'
 
 const routes: Routes = [
-  {// path: '', component: AppComponent },
+  //{ path: '', component: AppComponent },
 
 ];
 
