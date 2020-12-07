8f5a6b49aa739da6ec8aef1ca0990906c0e28346
madriesen <martijn.driesen5@hotmail.com>
2020-12-07 11:37:21 +0100

    add landingpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingpageComponent } from './landingpage/landingpage.component';
+
+const routes: Routes = [
+  { path: '', component: LandingpageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
