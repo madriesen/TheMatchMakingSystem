9821bea52dc36c849449b8f44f07f0333e1f3e29
madriesen <martijn.driesen5@hotmail.com>
2020-12-09 23:58:04 +0100

    fix env variables

--- src/app/dashboard/navigation/navigation.component.ts
+++ src/app/dashboard/navigation/navigation.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { faFutbol, faChess, faMapMarkerAlt, faChalkboardTeacher, faMedal } from '@fortawesome/free-solid-svg-icons';
 import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-navigation',
@@ -15,9 +17,14 @@ export class NavigationComponent implements OnInit {
   faChalkboardTeacher = faChalkboardTeacher;
   faMedal = faMedal;
   
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    // todo: implement logout
+    this.router.navigate(['/']);
   }
 
 }
