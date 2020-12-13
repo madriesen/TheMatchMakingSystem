00dfe8bdc16f9852d1982b6829b26a3d44c65b0a
Stijn Van Braband <r0740656@student.thomasmore.be>
2020-12-13 18:24:05 +0100

    admin-wedstrijd

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -17,6 +17,9 @@ import { EditTeamComponent } from './team/edit-team/edit-team.component';
 import { CompetitieComponent } from './competitie/competitie/competitie.component';
 import { AddCompetitieComponent } from './competitie/add-competitie/add-competitie.component';
 import { EditCompetitieComponent } from './competitie/edit-competitie/edit-competitie.component';
+import { WedstrijdComponent } from './wedstrijd/wedstrijd/wedstrijd.component';
+import { AddWedstrijdComponent } from './wedstrijd/add-wedstrijd/add-wedstrijd.component';
+import { EditWedstrijdComponent } from './wedstrijd/edit-wedstrijd/edit-wedstrijd.component';
 
 
 const appRoutes: Routes = [
@@ -34,7 +37,7 @@ const appRoutes: Routes = [
 ]
 
 @NgModule({
-  declarations: [AddUserComponent, AdminComponent, UserComponent, PloegComponent, AddPloegComponent, EditPloegComponent, TablesComponent, TeamComponent, AddTeamComponent, EditTeamComponent, CompetitieComponent, AddCompetitieComponent, EditCompetitieComponent],
+  declarations: [AddUserComponent, AdminComponent, UserComponent, PloegComponent, AddPloegComponent, EditPloegComponent, TablesComponent, TeamComponent, AddTeamComponent, EditTeamComponent, CompetitieComponent, AddCompetitieComponent, EditCompetitieComponent, WedstrijdComponent, AddWedstrijdComponent, EditWedstrijdComponent],
 
   imports: [
     CommonModule,
